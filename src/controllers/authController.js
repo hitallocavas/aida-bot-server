20543f44909c472a999cb592cef3db8806b41dc3
Matheus Lima <mal@cesar.school>
2019-02-08 10:33:10 -0300

    novo fluxo3

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -136,7 +136,7 @@ const router = express.Router();
 
                     if(prestadores.length==0 ){
                         res.send({
-                            "fulfillmentText": "Infelizmente não há registros de " + req.body.outputContexts.parameters.colaborador+ "com a especialidade "+req.body.queryResult.parameters.colaborador+ " em nosso banco de dados"
+                            "fulfillmentText": "Infelizmente não há registros de " + req.body.outputContexts.parameters.colaborador+ " em nosso banco de dados"
                         }
                         )
                     }
@@ -152,7 +152,7 @@ const router = express.Router();
                         {"fulfillmentMessages": [
                             {
                                 "text": {
-                                    "text": ["Aguarde um momento, estamos procurando os melhores "+req.body.queryResult.parameters.colaborador+"s pra você."]
+                                    "text": ["Aguarde um momento, estamos procurando os melhores "+req.body.outputContexts.parameters.colaborador+"s pra você."]
                                 }
                             },
                             {
