c77704132bf7a1bb657680b7fc71840c343f0627
Matheus Lima <mal@cesar.school>
2019-02-08 08:39:45 -0300

    2msg

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -157,7 +157,7 @@ const router = express.Router();
                             },
                             {
                                 "text": {
-                                    "text": ["dskljdhsdfjhsdkfdshfsdk"]
+                                    "text": ["Aqui estão os "+req.body.queryResult.parameters.colaborador+"que você pediu. Se precisar de mais alguma coisa, me avise."]
                                 }
                             }
                           ]   
